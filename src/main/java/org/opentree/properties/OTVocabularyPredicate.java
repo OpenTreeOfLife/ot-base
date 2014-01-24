b598fb33a62a382978b12a097c02e3fd543b087d
Cody Hinchliff <cody.hinchliff@gmail.com>
2014-01-24 14:53:06 -0500

    more info on properties, some corrections

--- src/main/java/org/opentree/properties/OTVocabularyPredicate.java
+++ src/main/java/org/opentree/properties/OTVocabularyPredicate.java
@@ -8,29 +8,59 @@ package org.opentree.properties;
  */
 public enum OTVocabularyPredicate implements OTPropertyPredicate {
 	
+	/**
+	 * Node property
+	 */
 	OT_AGE ("ot:age", Double.class),
 	
+	/**
+	 * Node property
+	 */
 	OT_AGE_MIN ("ot:ageMin", Double.class),
 	
+	/**
+	 * Node property
+	 */
 	OT_AGE_MAX ("ot:ageMax", Double.class),
 	
+	/**
+	 * Study property
+	 */
 	OT_AUTHOR_CONTRIBUTED ("ot:authorContributed", OTVocabularyObject.class),
 	
+	/**
+	 * Tree property
+	 */
 	OT_BRANCH_LENGTH_DESCRIPTION ("ot:branchLengthDescription", String.class),
 	
+	/**
+	 * Tree property
+	 */
 	OT_BRANCH_LENGTH_TIME_UNITS ("ot:branchLengthTimeUnits", String.class),
 	
 	/**
-	 * The type of branch lengths for this tree. A property of tree root nodes.
+	 * Tree property. The type of branch lengths for this tree.
 	 */
 	OT_BRANCH_LENGTH_MODE("ot:branchLengthMode", String.class),
 	
+	/**
+	 * Node property
+	 */
 	OT_NODE_LABEL ("ot:nodeLabel", String.class),
 	
+	/**
+	 * Tree property. Describes the type of information stored in node labels.
+	 */
 	OT_NODE_LABEL_DESCRIPTION ("ot:nodeLabelDescription", String.class),
 	
+	/**
+	 * Tree property.
+	 */
 	OT_NODE_LABEL_MODE ("ot:nodeLabelMode", OTVocabularyObject.class),
-	
+
+	/**
+	 * General property
+	 */
 	OT_COMMENT ("ot:comment", String.class),
 	
 	/**
@@ -48,6 +78,9 @@ public enum OTVocabularyPredicate implements OTPropertyPredicate {
 	 */
 	OT_FOCAL_CLADE ("ot:focalClade", Long.class),
 	
+	/**
+	 * Tree property. Describes the way the tree was inferred.
+	 */
 	OT_INFERENCE_METHOD ("ot:inferenceMethod", String.class),
 	
 	/**
@@ -57,25 +90,34 @@ public enum OTVocabularyPredicate implements OTPropertyPredicate {
 	 */
 	OT_INGROUP_CLADE("ot:inGroupClade", String.class),
 	
+	/**
+	 * Node property. Is this node the ingroup for this tree?
+	 */
 	OT_IS_INGROUP ("ot:isIngroup", Boolean.class), // TODO: switch over to this term for the otu labels currently using the OTUNodeProperty term
-	
+
+	/**
+	 * Node property. Is this node a leaf in the tree?
+	 */
 	OT_IS_LEAF ("ot:isLeaf", Boolean.class), // TODO: switch over to this term for the otu labels currently using the OTUNodeProperty term
 	
 	/**
-	 * The original label assigned to this node.
+	 * Node (otu) property. The original label assigned to this node.
 	 */
 	OT_ORIGINAL_LABEL ("ot:originalLabel", String.class),	
 
 	/**
-	 * The ott id associated with the node. A property of tip nodes. Should not be set if the node has not been mapped.
+	 * Node (otu) property. The ott id associated with the taxon assigned to this node. Should not be set if the node has not been mapped.
 	 */
 	OT_OTT_ID ("ot:ottId", Long.class),
 	
 	/**
-	 * The ottol name associated with this taxon
+	 * Node (otu) property. The ott taxon name associated with this taxon.
 	 */
 	OT_OTT_TAXON_NAME ("ot:ottTaxonName", String.class),
-	
+
+	/**
+	 * Node property.
+	 */
 	OT_PARENT ("ot:parent", String.class),
 	
 	/**
@@ -83,6 +125,9 @@ public enum OTVocabularyPredicate implements OTPropertyPredicate {
 	 */
     OT_PUBLICATION_REFERENCE ("ot:studyPublicationReference", String.class),
 
+    /**
+     * A tree property. The nexson id of the node that is the specified root node for the tree. Should always be the deepest node in the tree (by convention/nexson spec).
+     */
     OT_SPECIFIED_ROOT ("ot:specifiedRoot", String.class),
     
     /**
@@ -90,30 +135,54 @@ public enum OTVocabularyPredicate implements OTPropertyPredicate {
      */
     OT_STUDY_ID ("ot:studyId", String.class),
     
+    /**
+     * Study property.
+     */
     OT_STUDY_LABEL ("ot:studyLabel", String.class),
     
+    /**
+     * Study property.
+     */
     OT_STUDY_LAST_EDITOR ("ot:studyLastEditor", String.class),
     
+    /**
+     * Study property. A datetime string. Format?
+     */
     OT_STUDY_MODIFIED ("ot:studyModified", String.class), // a datetime string
-    
-    OT_STUDY_UPLOADED ("ot:studyUploaded", String.class), // a datetime string
     
     /**
      * A URI (or other identifier) for published studies. A property of source meta nodes.
      */
     OT_STUDY_PUBLICATION("ot:studyPublication", String.class),
-    
+
+    /**
+     * Study property. A datetime string. Format?
+     */
+    OT_STUDY_UPLOADED ("ot:studyUploaded", String.class), // a datetime string
+
     /**
      * The tag field exported from phylografter. A property of source meta nodes.
      */
     OT_TAG ("ot:tag", String.class),
     
+    /**
+     * Tree property.
+     */
     OT_TREEBASE_TREE_ID ("ot:treebaseTreeId", String.class),
     
-    OT_TREEBASE_OTU_ID ("ot:treebaseOTUIid", String.class),
+    /**
+     * Node (otu) property.
+     */
+    OT_TREEBASE_OTU_ID ("ot:treebaseOTUId", String.class),
     
+    /**
+     * Tree property. A datetime string.
+     */
     OT_TREE_LAST_EDITED ("ot:treeLastEdited", String.class), // a datetime string
     
+    /**
+     * Tree property. A datetime string.
+     */
     OT_TREE_MODIFIED ("ot:treeModified", String.class), // a datetime string
     
     /**
