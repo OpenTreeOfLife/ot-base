d2bf18ccd0ff6880c8c8a73612b7cf6dd714c413
Cody Hinchliff <cody.hinchliff@gmail.com>
2014-09-23 12:50:30 -0400

    minor cleanup

--- src/main/java/org/opentree/utils/GeneralUtils.java
+++ src/main/java/org/opentree/utils/GeneralUtils.java
@@ -88,4 +88,17 @@ public class GeneralUtils {
 	    String cleanName = dirtyName.replaceAll(offendingChars, "_");	    
 	    return cleanName;
 	}
+
+	/**
+	 * A function for converting string arrays to HashSets of strings, provided as a convenience for preparing string arrays for TNRS.
+	 * @param input strings
+	 * @return a set of Strings containing the input
+	 */
+	public static HashSet<String> stringArrayToHashset(String[] strings) {
+	    HashSet<String> stringSet = new HashSet<String>();
+	    for (int i = 0; i < strings.length; i++) {
+	        stringSet.add(strings[i]);
+	    }
+	    return stringSet;
+	}
 }
