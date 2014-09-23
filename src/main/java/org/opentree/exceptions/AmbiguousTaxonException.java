fb9dc1d96b10a7c934307a20989ef37916eb0fac
Cody Hinchliff <cody.hinchliff@gmail.com>
2014-09-23 15:25:22 -0400

    fixing package name error

--- src/main/java/org/opentree/exceptions/AmbiguousTaxonException.java
+++ src/main/java/org/opentree/exceptions/AmbiguousTaxonException.java
@@ -1,7 +1,5 @@
 package org.opentree.exceptions;
 
-import opentree.exceptions.MultipleHitsException;
-
 /**
  * This class is just a wrapper for MultipleHitsException, but is reserved for the case when we find multiple
  * hits for a taxon search where we were expecting only one (e.g. a search for a tax_uid or taxon name).
